d66bf9158ec005829431e991721c5f24d98af3d1
Seven Lju <dna2gle@gmail.com>
2021-07-20 18:36:03 +0800

    add loading mask

--- src/client/logic/global.ts
+++ src/client/logic/global.ts
@@ -1,5 +1,24 @@
 import {TaskRunner} from './task';
 
 export const Env: any = {
-   task: new TaskRunner()
+   task: new TaskRunner(),
+   mask: {
+      show: (zIndex: number = 0) => {
+         let div = document.querySelector('.mask');
+         if (!div) {
+            div = document.createElement('div');
+            div.className = 'mask';
+            document.body.appendChild(div);
+         }
+         div.style.display = 'block';
+         if (zIndex) {
+            div.style.zIndex = zIndex;
+         }
+      },
+      hide: () => {
+         const div = document.querySelector('.mask');
+         if (!div) return;
+         div.style.display = 'none';
+      }
+   }
 };
