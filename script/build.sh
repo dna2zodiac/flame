88bf3594deab84f93c9dce696884faf8ab2d4bb3
Seven Lju <dna2gle@gmail.com>
2021-07-31 19:48:51 +0800

    add side favicon

--- script/build.sh
+++ script/build.sh
@@ -8,5 +8,6 @@ rm -rf $SELF/dist
 cd $SELF
 ./node_modules/.bin/webpack
 cp src/client/index.html dist/static/
+cp src/client/favicon.ico dist/static/
 mkdir -p dist/static/img/
 cp src/client/img/* dist/static/img/
