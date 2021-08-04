f39c3e23183561cd493343c98f3ad5106a31d967
Seven Lju <dna2gle@gmail.com>
2021-08-04 09:33:45 +0800

    init search indexer basic functions

--- src/share/lang/common.ts
+++ src/share/lang/common.ts
@@ -9,13 +9,24 @@ export const STOPS = [
 export class BasicTextParser {
    constructor() {}
 
-   Parse(text: string): string[] {
+   Parse(text: string, opt: any): string[] {
+      /* opt:
+         - noKeepStop: not store stop characters into result list
+         - nonAsciiWord: treat non-ascii character as a word
+       */
       const r: string[] = [];
       const n = text.length;
+      opt = opt || {};
       let cache = '';
       for (let i = 0; i < n; i++) {
          const ch = text.charAt(i);
          if (STOPS.includes(ch)) {
+            if (cache) {
+               r.push(cache);
+               cache = '';
+            }
+            if (opt.noKeepStop) r.push(ch);
+         } else if (opt.nonAsciiWord && ch.charCodeAt(0) >= 128) {
             if (cache) {
                r.push(cache);
                cache = '';
