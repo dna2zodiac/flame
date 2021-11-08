b794d9bdbc4bfa6229df96ccd29cc9cd56cf432f
Seven Lju <dna2gle@gmail.com>
2021-11-08 19:07:14 +0800

    print stage info of build proress

--- script/build.sh
+++ script/build.sh
@@ -7,6 +7,8 @@ SELF=$(cd `dirname $0`/..; pwd)
 rm -rf $SELF/dist
 cd $SELF
 ./node_modules/.bin/webpack
+
+echo copy static files ...
 cp src/client/index.html dist/static/
 cp src/client/favicon.ico dist/static/
 mkdir -p dist/static/img/
