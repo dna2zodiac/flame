31b1726c62c50455e3274d4cd778e04cc430b8fe
Seven Lju <dna2gle@gmail.com>
2025-02-09 11:18:42 +0800

    vscode: add status bar item

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -15,6 +15,13 @@ export function activate(context: vscode.ExtensionContext) {
       FlameCodeNavigator.createOrShow(context.extensionUri);
    });
    gc.record(context, cmdCodeFlameShow, 'cmd:codeflame.show');
+
+   const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+   statusBarItem.text = "$(preview) Flame";
+   statusBarItem.tooltip = new vscode.MarkdownString(`**Show** Flame Code Navigator`);
+   statusBarItem.command = 'codeflame.show';
+   statusBarItem.show();
+   gc.record(context, statusBarItem, 'status:item');
 }
 
 export function deactivate() {
