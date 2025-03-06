dd7efb27db2e0052fe048992b8a6e600114a7d75
Seven Lju <dna2gle@gmail.com>
2025-03-06 23:30:51 +0800

    flame: nextui - add bootstrap icon & init left side navbar

--- nextui/src/App.vue
+++ nextui/src/App.vue
@@ -5,16 +5,87 @@ import { initEditor } from './monaco-editor'
 const monacoEditor = ref(null);
 
 onMounted(() => {
-   console.log(monacoEditor);
    initEditor(monacoEditor.value);
 });
 </script>
 
 <template>
-  <div ref="monacoEditor" class="editor"></div>
+  <div class="view">
+    <div class="navbar">
+      <div><button @click="switchTab(1)"><i class="bi bi-search"/></button></div>
+      <div><button @click="switchTab(2)"><i class="bi bi-stack"/></button></div>
+      <div><button @click="switchTab(3)"><i class="bi bi-grid-3x3-gap-fill"/></button></div>
+      <div><button @click="switchTab(4)"><i class="bi bi-file-text-fill"/></button></div>
+    </div>
+    <div class="workspace">
+      <div class="main-view">
+        <div :class="{'tab-container': true, hide: tab == 0}">
+          <div :class="{hide: tab != 1}">Search</div>
+          <div :class="{hide: tab != 2}">ViewStack</div>
+          <div :class="{hide: tab != 3}">CallGrid</div>
+          <div :class="{hide: tab != 4}">LogMap</div>
+        </div>
+        <div class="editor-container">
+          <div ref="monacoEditor" class="editor"></div>
+        </div>
+      </div>
+      <div>Status</div>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      tab: 0,
+    };
+  },
+  methods: {
+    switchTab(index) {
+      if (this.tab == index) {
+        this.tab = 0;
+      } else {
+        this.tab = index;
+      }
+    }
+  },
+}
+</script>
+
 <style scoped>
+.view {
+   display: flex;
+   flex-direction: row;
+   width: 100%;
+   height: 100%;
+   overflow: hidden;
+}
+.navbar {
+   margin: 10px 0 10px 10px;
+}
+.workspace {
+   display: flex;
+   flex: 1 0 auto;
+   flex-direction: column;
+   width: 100%;
+   height: 100%;
+}
+.main-view {
+  flex: 1 0 auto;
+  width: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+}
+.tab-container {
+  width: 40%;
+  padding: 10px;
+  flex: 0 0 auto;
+}
+.editor-container {
+   flex: 1 0 auto;
+}
 .editor {
    width: 100%;
    height: 100%;
